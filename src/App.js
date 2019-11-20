e1522e85cbaf2cb4158ec6d44d94851f0134aee7
Ashish Thapa <legendashish66@gmail.com>
2019-11-20 08:12:51 +0545

    Function after using Hook

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; 
 import Person from './Person/Person';
 
-class App extends Component {
+const App = (props) => {
 
-  state =
-  {
+  const [personState,setPersonState] = useState({
     persons:[
       {name:'Ashish',age:21},
       {name:'Sudip',age:20},
     ]
-  }
+  });
 
-  switchNameHandler = () =>{
+  const [otherState,setOtherState]=useState('some other value');
+
+  console.log(personState,otherState);
+
+const switchNameHandler = () =>{
     // console.log('something changed.');
     //Don't do this... this.state.persons[0].name='Suraj';
-   this.setState({
+   setPersonState({
      persons:[{name:'Suraj',age:21},
      {name:'Sudip',age:21},]
    }
    )
   }
-   render(){
      return(
        <div className="App">
          <h1>Hi, I'm a React App</h1>
          <p>Check this is working</p>
          <p>My favourite fruit is mango.</p>
-         <button onClick={this.switchNameHandler}>Random names</button>
-         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>okay</Person>
-         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>haha</Person>
+         <button onClick={switchNameHandler}>Random names</button>
+         <Person name={personState.persons[0].name} age={personState.persons[0].age}>okay</Person>
+         <Person name={personState.persons[1].name} age={personState.persons[1].age}>haha</Person>
       </div>
            );
     //   return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Does it work now')); 
-    }
 }
 
 export default App;
